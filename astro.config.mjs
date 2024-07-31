e1f729a937caa02b0c213dbd33e715dc4dfe12c5
Luchian Marc <marc.luchian@bit-sentinel.com>
2024-07-31 21:54:23 +0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://lumablog.com",
+  site: "https://qLuma.github.io",
   integrations: [mdx(), sitemap(), tailwind()],
 });
