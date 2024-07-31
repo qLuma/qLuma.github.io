0dd8d68a1bfb299f9820d4eb284ca69770186126
Luchian Marc <marc.luchian@bit-sentinel.com>
2024-07-31 21:47:10 +0300

    CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://qLuma.github.io",
+  site: "https://lumablog.com",
   integrations: [mdx(), sitemap(), tailwind()],
 });
